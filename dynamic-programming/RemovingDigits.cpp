2f61b52d5301c65892135b7a6030cec95475fe4e
yash7xm <yash.7xm@gmail.com>
2025-07-22 16:58:04 +0530

    add removing digits

--- dynamic-programming/RemovingDigits.cpp
+++ dynamic-programming/RemovingDigits.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int getLargestDigit(int num) {
-    int largest = 0;
-    while (num > 0) {
-        int digit = num % 10;
-        largest = max(largest, digit);
-        num /= 10;
-    }
-    return largest;
-}
-
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    
-    int steps = 0;
-    while (n > 0) {
-        int digit = getLargestDigit(n);
-        n -= digit;
-        steps++;
+
+    vector<int> dp(n+1, INT_MAX);
+    dp[0] = 0;
+
+    for(int i=1; i<=n; ++i) {
+    	int num = i;
+    	while(num > 0) {
+    		int digit = num % 10;
+    		if(digit != 0 && i-digit >= 0) {
+    			dp[i] = min(dp[i], dp[i-digit] + 1);
+    		}
+    		num /= 10;
+    	}
     }
-    
-    cout << steps << endl;
-    
+
+    cout << dp[n] << endl;
+
     return 0;
 }
