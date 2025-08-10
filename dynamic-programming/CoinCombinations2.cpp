9dff1bccc8e0045650c01483089de939a3260c7c
yash7xm <yash.7xm@gmail.com>
2025-08-10 10:28:10 +0530

    add Coin Combinations 2

---
+++ dynamic-programming/CoinCombinations2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> coins(n);
+    for(int& coin : coins) {
+    	cin >> coin;
+    }
+
+    vector<long long> dp(x + 1, 0);
+
+    dp[0] = 1;
+
+    for(int coin : coins) {
+    	for(int i=coin; i <= x; ++i) {
+    		dp[i] = (dp[i] + dp[i-coin]) % mod;
+    	}
+    }
+
+    cout << dp[x] << endl;
+
+    return 0;
+}
