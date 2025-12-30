3951dacb18634fc9055e90856f89f22affca717d
Yash Poonia <yash.7xm@gmail.com>
2025-12-30 16:30:59 +0530

    add Count Good Numbers

---
+++ maths/CountGoodNumbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    const long long MOD = 1e9 + 7;
+
+    long long modPow(long long base, long long exp) {
+        long long result = 1;
+        base %= MOD;
+
+        while (exp > 0) {
+            if (exp & 1)
+                result = (result * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long evenCount = (n + 1) / 2;
+        long long oddCount = n / 2;
+
+        long long ans = (modPow(5, evenCount) * modPow(4, oddCount)) % MOD;
+
+        return ans % MOD;
+    }
+};
