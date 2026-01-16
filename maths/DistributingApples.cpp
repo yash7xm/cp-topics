e40d9ade82a998fe6baf36c98cee8e7945e52a11
Yash Poonia <yash.7xm@gmail.com>
2026-01-16 18:51:27 +0530

    add Two Arrays

--- maths/DistributingApples.cpp
+++ maths/DistributingApples.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 static const long long MOD = 1000000007;
-static const int MAXN = 2005;
+static const int MAXN = 2000000;
 
-long long fact[MAXN], invfact[MAXN];
+long long fact[MAXN + 1], invfact[MAXN + 1];
 
 long long modpow(long long a, long long b) {
     long long res = 1;
@@ -18,14 +18,12 @@ long long modpow(long long a, long long b) {
 
 void precompute() {
     fact[0] = 1;
-    for (int i = 1; i < MAXN; i++) {
+    for (int i = 1; i <= MAXN; i++)
         fact[i] = fact[i - 1] * i % MOD;
-    }
 
-    invfact[MAXN - 1] = modpow(fact[MAXN - 1], MOD - 2);
-    for (int i = MAXN - 1; i > 0; i--) {
+    invfact[MAXN] = modpow(fact[MAXN], MOD - 2);
+    for (int i = MAXN; i > 0; i--)
         invfact[i - 1] = invfact[i] * i % MOD;
-    }
 }
 
 long long nCr(int n, int r) {
@@ -42,10 +40,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int total = n + 2 * m - 1;
-    long long ans = nCr(total, 2 * m);
-
-    cout << ans << "\n";
+    cout << nCr(m + n - 1, n - 1) << "\n";
     return 0;
 }
 
