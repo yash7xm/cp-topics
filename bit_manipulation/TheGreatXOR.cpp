e621248c1f119b31b0708a621f9da086823092ef
Yash Poonia <yash.7xm@gmail.com>
2026-03-14 23:08:50 +0530

    add The Great XOR

--- bit_manipulation/TheGreatXOR.cpp
+++ bit_manipulation/TheGreatXOR.cpp
@@ -20,6 +20,45 @@ void solve() {
   cout << count << "\n";
 }
 
+void solve_OR() {
+  ll x;
+  cin >> x;
+
+  ll num_of_bits = 64 - __builtin_clzll(x);
+
+  ll res = 0;
+
+  for(int i = 0; i < num_of_bits; ++i) {
+    if((x & (1LL << i)) == 0) {
+      res |= (1LL << i);
+    }
+  }
+
+  cout << count << "\n";
+}
+
+void solve_xor() {
+  ll x;
+  cin >> x;
+
+  ll num_of_bits = 64 - __builtin_clzll(x);
+
+  ll full_mask = (1LL << num_of_bits) - 1;
+
+  cout << x ^ full_mask << "\n"; 
+}
+
+void solve_diff() {
+  ll x;
+  cin >> x;
+
+  ll num_of_bits = 64 - __builtin_clzll(x);
+
+  ll full_mask = (1LL << num_of_bits) - 1;
+
+  cout << full_mask - x << "\n"; 
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
